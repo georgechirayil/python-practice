03f00047840218006ccdd8a9d907423ff88b6a27
georgechirayil <noah.chirayil@gmail.com>
2025-06-25 15:50:42 -0400

    Create p3-rockpaperscissors.py

---
+++ src/p3-rockpaperscissors.py
@@ -0,0 +1,22 @@
+choice = "paper"  # Computer chooses paper.
+print("rock-paper-scissors: type here:     ")
+player = input ()
+if player == choice: 
+    print ("Game is a tie. Please try again.")
+if player == "rock":
+if choice == "scissors":
+    print ("Congratulations. You win.")
+else:
+      print ("Sorry - Computer wins.")
+elif player == "paper":
+if choice == "rock":
+    print ("Sorry.You lose.")
+else:
+    print ("aaxčesW. You win.")
+elif player == "scissors"
+if choice == "rock":
+    print ("Sorry computer wins.")
+else: 
+     print ("Congratulations You win.")
+else:
+    print ("Error: select one of: rock,paper,sissors")
