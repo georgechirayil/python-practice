27ce40489bb25ad15a832ab1c9cc7828c82f716a
georgechirayil <noah.chirayil@gmail.com>
2025-06-27 15:36:59 -0400

    Update p3-rockpaperscissors.py
    Fixed logic

--- src/p3-rockpaperscissors.py
+++ src/p3-rockpaperscissors.py
@@ -1,16 +1,19 @@
 choice = "paper"  # Computer chooses paper.
 print("rock-paper-scissors: type here:     ")
 player = input ()
-if player == choice: 
+
+while player == choice:
     print ("Game is a tie. Please try again.")
+    
+player = input()
 if player == "rock":
     if choice == "scissors":
         print ("Congratulations. You win.")
     else:
         print ("Sorry - Computer wins.")
 elif player == "paper":
-    if choice == "rock":
-        print ("Sorry.You lose.")
+    if choice == "scissors":
+        print ("Sorry - computer wins.")
     else:
         print ("Congratulations. You win.")
 elif player == "scissors":
